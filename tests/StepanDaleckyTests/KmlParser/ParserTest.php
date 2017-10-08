33811867c0f4b66b4f23b47ceb3ac0bc6bc19f9b
Štěpán Dalecký <stepan.dalecky@gmail.com>
2017-10-08 22:23:02 +0200

    Prepare environment

---
+++ tests/StepanDaleckyTests/KmlParser/ParserTest.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace StepanDaleckyTests\KmlParser;
+
+use PHPUnit\Framework\TestCase;
+
+class ParserTest extends TestCase
+{
+
+	public function test()
+	{
+		self::assertTrue(true);
+	}
+}
